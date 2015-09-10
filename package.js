a8ce6afcfa264c96047dfd1d78163d85e960b134
harmoN <dougwright1@gmail.com>
2015-09-10 10:52:02 -0400

    add testing files

---
+++ package.js
@@ -0,0 +1,25 @@
+Package.describe({
+  name: 'harmon:meteor-josh-js',
+  version: '0.0.1',
+  // Brief, one-line summary of the package.
+  summary: '',
+  // URL to the Git repository containing the source code for this package.
+  git: '',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.1.0.3');
+  api.use('underscore');
+  api.use('jquery');
+  api.addFiles(['client/history.js','client/killring.js','client/readline.js','client/input.js','client/shell.js', 'client/pathhandler.js', 'client/meteor-josh-js.js'], 'client');
+  api.export('Shell', 'client')
+});
+
+Package.onTest(function(api) {
+  api.use('tinytest');
+  api.use('harmon:meteor-josh-js', 'client');
+  api.addFiles('tests/client/meteor-josh-js-tests.js', 'client');
+});
---
+++ tests/client/meteor-josh-js-tests.js
@@ -0,0 +1,5 @@
+// Write your tests here!
+// Here is an example.
+Tinytest.add('is shell loaded', function (test) {
+  test.isNotUndefined(Shell, 'Shell');
+});
